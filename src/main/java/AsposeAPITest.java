8debc9cdf084b8eb78921841e66b3503c2c558a9
adeel.ilyas@aspose.com <adeel.ilyas@aspose.com>
2017-12-18 11:22:16 +0500

    Dynabic.Artifactory Usage Sample Project
    Signed-off-by: adeel.ilyas@aspose.com <adeel.ilyas@aspose.com>

---
+++ src/main/java/AsposeAPITest.java
@@ -0,0 +1,30 @@
+import com.aspose.words.*;
+
+import java.util.Iterator;
+
+/**
+ * Created by Adeel Ilyas on 6/27/2014.
+  * Updated on 12.18.2017 for Dynabic.Artifactory by Adee Ilyas
+ */
+
+/*
+
+ Aspose.Words Example
+ *
+ */
+public class AsposeAPITest {
+
+    public static void main( String args[]) throws Exception {
+        final String WordOutPutFile = "output/AsposeMaven.doc";
+        System.out.println("Welcome to Aspose For Maven!");
+
+        try {
+            AsposeJavaExamples.runAsposeWord(WordOutPutFile);
+        } catch (Exception e) {
+            throw new Exception("Aspose: Unable to export to ms word format.. some error occured",e);
+
+        }
+
+    }
+
+}
